8c49857190b653aa2a5c14b06d1c304d939064f4
Ville Heikkinen <ville@ubuntu.(none)>
2014-01-23 13:11:52 +0200

    excercises 4-5

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        avg = 0;
+        ratings.each { |rating|
+            avg += rating.score
+        }
+        return avg / ratings.length
+    end
 end
