42acd9446d30a1299e836a4ff81abc58f0a38cb1
Ville Heikkinen <ville@ubuntu.(none)>
2014-01-25 17:01:25 +0200

    excer 3

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Ratebeer::Application.routes.draw do
    
   get 'signup', to:'users#new'
 
+  get 'signin', to:'sessions#new'
 
+  delete 'signout', to:'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
